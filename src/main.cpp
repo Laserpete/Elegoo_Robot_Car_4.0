bf722f52211632141a09dcd94291b5e04256b5ab
LaserPete <pete.t.doyle@gmail.com>
2021-04-07 18:41:37 +0200

    Make IR driving control work

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,32 @@
 #define TURN_SPOT_F_OF_X_B 40
 
 // Driving forward and back trim
-#define STEERING_TRIM_VAL 20
+#define FORWARD_STEERING_TRIM 0
+#define BACKWARD_STEERING_TRIM 20
+// IR Control turn amount in degrees
+#define IR_TURN_MAGNITUDE_DEGREES 20
+
 #define MILLISECONDS 1000
+
+// IR control commands on NEC protocol in HEX
+
+#define IR_FORWARD 0x46
+#define IR_BACKWARD 0x15
+#define IR_LEFT 0x44
+#define IR_RIGHT 0x43
+#define IR_OK 0x40
+#define IR_1 0x16
+#define IR_2 0x19
+#define IR_3 0xD
+#define IR_4 0xC
+#define IR_5 0x18
+#define IR_6 0x5E
+#define IR_7 0x8
+#define IR_8 0x1C
+#define IR_9 0x5A
+#define IR_0 0x52
+#define IR_ASTERISK 0x42
+#define IR_HASH 0x4A
 
 int AIN1 = 8;  // direction low = forward
 int AIN2 = 5;  // speed PWM
@@ -50,7 +74,7 @@ void setup() {
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   Serial.println(F("START " __FILE__ " from "__DATE__
-                   "\r\nUsing library version " VERSION_IRREMOTE));
+                   "\r\nUsing IR Remote library version " VERSION_IRREMOTE));
   IrReceiver.begin(IR_RECEIVE_PIN, false);
 }
 
@@ -81,7 +105,7 @@ void forward(int d, float velocity) {
   // Calculate wheel PWM value and et steering trim
 
   leftSpeed = rightSpeed = calcWheelPWM(velocity);
-  rightSpeed = leftSpeed - STEERING_TRIM_VAL;
+  rightSpeed = leftSpeed - FORWARD_STEERING_TRIM;
 
   // Set wheel PWM values
   analogWrite(AIN2, leftSpeed);
@@ -98,7 +122,7 @@ void backward(int d, float velocity) {
 
   // Calculate wheel PWM value and et steering trim
   leftSpeed = rightSpeed = calcWheelPWM(velocity);
-  rightSpeed = leftSpeed + STEERING_TRIM_VAL;
+  rightSpeed = leftSpeed + BACKWARD_STEERING_TRIM;
 
   // Set wheel PWM values
   analogWrite(AIN2, leftSpeed);
@@ -109,13 +133,12 @@ void backward(int d, float velocity) {
   delay(t * MILLISECONDS);
 }
 
-void stopCar(int d) {
+void stopCar() {
   digitalWrite(AIN2, LOW);
   digitalWrite(BIN2, LOW);
-  delay(d);
 }
 void turnLeft(int angle) {
-  stopCar(500);
+  stopCar();
   digitalWrite(AIN1, LOW);
   digitalWrite(BIN1, LOW);
   analogWrite(AIN2, TURN_LR_PWM);
@@ -125,7 +148,7 @@ void turnLeft(int angle) {
 }
 
 void turnRight(int angle) {
-  stopCar(500);
+  stopCar();
   digitalWrite(AIN1, HIGH);
   digitalWrite(BIN1, HIGH);
   analogWrite(AIN2, TURN_LR_PWM);
@@ -171,22 +194,68 @@ int calculateMaximumSpeed(int CMpS) {
   return maxCMpS;
 }
 
+void IrControlInterpreter() {
+  IrReceiver.printIRResultShort(&Serial);
+  int IrCommand = IrReceiver.decodedIRData.command;
+  IrReceiver.resume();
+
+  switch (IrCommand) {
+    case IR_FORWARD:
+      forward(25, 50);
+      stopCar();
+      break;
+
+    case IR_BACKWARD:
+      backward(25, 50);
+      stopCar();
+      break;
+
+    case IR_LEFT:
+      turnLeft(IR_TURN_MAGNITUDE_DEGREES);
+      stopCar();
+      break;
+
+    case IR_RIGHT:
+      turnRight(IR_TURN_MAGNITUDE_DEGREES);
+      stopCar();
+      break;
+
+    case IR_0:
+    case IR_1:
+    case IR_2:
+    case IR_3:
+    case IR_4:
+    case IR_5:
+    case IR_6:
+    case IR_7:
+    case IR_8:
+    case IR_9:
+    case IR_ASTERISK:
+    case IR_HASH:
+      break;
+  }
+}
+
 void loop() {
   // Serial.println("This is working");
 
   if (IrReceiver.decode()) {
-    IrReceiver.printIRResultShort(&Serial);
-    Serial.println();
-    IrReceiver.resume();
-
-    if (IrReceiver.decodedIRData.command == 0x10) {
-      Serial.println("Do something");
-    } else if (IrReceiver.decodedIRData.command == 0x11) {
-      Serial.println("Do something else");
-    }
+    IrControlInterpreter();
   }
-  delay(500);
+  /*
+  IrReceiver.printIRResultShort(&Serial);
+  Serial.println(IrReceiver.decodedIRData.command, HEX);
+  Serial.println();
+  IrReceiver.resume();
 
+  if (IrReceiver.decodedIRData.command == 0x10) {
+    Serial.println("Do something");
+  } else if (IrReceiver.decodedIRData.command == 0x11) {
+    Serial.println("Do something else");
+  }
+}
+delay(500);
+*/
   /*
   leftSpeed = 100;
   rightSpeed = 100;
